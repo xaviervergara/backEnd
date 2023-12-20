7cc708beead2fa7bfc721f69501e324a48d3214f
xaviervergara <xaviervergara00@gmail.com>
2023-12-20 00:10:46 -0300

    actividad actualizada

--- actividad_4/app.js
+++ actividad_4/app.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const http = require('http'); // Importa el módulo http
+const socketIO = require('socket.io'); // Importa el módulo socket.io
+const exphbs = require('express-handlebars'); // Importa el módulo express-handlebars
 const fs = require('fs');
 const app = express();
+const server = http.createServer(app); // Crea el servidor HTTP usando express
+const io = socketIO(server); // Crea el servidor de socket.io utilizando el servidor HTTP
+
 const PORT = 8080;
 const ProductManager = require('./classes/productManager');
 const CartManager = require('./classes/cartManager');
@@ -9,15 +15,49 @@ const cartsRouter = require('./carts.router');
 
 app.use(express.json());
 
-// Montar el router de productos en la ruta /api/products
+// Configura Handlebars como el motor de plantillas
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+// Monta el router de productos en la ruta /api/products
 app.use('/api/products', productsRouter);
 
-// Montar el router de carritos en la ruta /api/carts
+// Monta el router de carritos en la ruta /api/carts
 app.use('/api/carts', cartsRouter);
 
 // Crear instancias de las clases
 const productManager = new ProductManager('./products.json');
 const cartManager = new CartManager('./carts.json');
+
+productManager.addProduct({
+  title: 'Producto 1',
+  description: 'Descripción del Producto 1',
+  code: 'P1',
+  price: 19.99,
+  stock: 50,
+});
+
+productManager.addProduct({
+  title: 'Producto 2',
+  description: 'Descripción del Producto 2',
+  code: 'P2',
+  price: 29.99,
+  stock: 30,
+});
+
+// Ruta para la vista en tiempo real
+app.get('/realtimeproducts', (req, res) => {
+  // Renderiza la vista realTimeProducts.handlebars
+  res.render('realTimeProducts');
+});
+
+// Escucha eventos de conexión de sockets
+io.on('connection', (socket) => {
+  console.log('Nuevo cliente conectado');
+
+  // Puedes agregar lógica de manejo de eventos de sockets aquí
+  // Por ejemplo, cuando un nuevo producto se agrega, puedes emitir un evento a los clientes conectados para actualizar la lista en tiempo real.
+});
 
 // root del servidor
 app.get('/', (req, res) => {
@@ -25,6 +65,6 @@ app.get('/', (req, res) => {
 });
 
 // Iniciar el servidor
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
